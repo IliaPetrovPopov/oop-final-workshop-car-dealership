799cb8c5128e9ec5f50159444e61120eac8ea3d2
Ilia Popov <ilia_popov02@abv.bg>
2023-06-03 16:54:19 +0300

    Add implementation for child class Car

--- Template/src/Models/Car.js
+++ Template/src/Models/Car.js
@@ -1,4 +1,7 @@
 export class Car {
+	#seats;
+	#vehicleSpecifics;
+
 	static SEATS_MIN_VALUE = 1;
 	static SEATS_MAX_VALUE = 10;
 	static SEATS_ERROR_MESSAGE = `Seats must be between ${Car.SEATS_MIN_VALUE} and ${Car.SEATS_MAX_VALUE}!`;
@@ -13,8 +16,34 @@ export class Car {
 	 * @param {number} seats - The number of seats in the car.
 	 */
 	constructor(make, model, price, seats) {
-		// TODO: Implement the constructor
+	  super(make, model, price);
+	  this.determineTypeAndWheels();
+	  this.seats = seats;
+	  this.#vehicleSpecifics = `Seats: ${this.#seats}`;
 	}
 
-	// TODO: Implement the rest of the class
+	get seats() {
+	  return this.#seats;
+	}
+
+	set seats(newSeats) {
+
+	  if (!newSeats && newSeats !== 0) {
+	    throw new Error('Please enter valid value!');
+	  }
+
+	  if (newSeats < Car.SEATS_MIN_VALUE || newSeats > Car.SEATS_MAX_VALUE) {
+	    throw new Error(Car.SEATS_ERROR_MESSAGE);
+	  }
+
+	  this.#seats = newSeats;
+	}
+
+	get vehicleSpecifics() {
+	  return this.#vehicleSpecifics;
+	}
+
+	additionalInfo() {
+	  return this.vehicleSpecifics;
+	}
 }
