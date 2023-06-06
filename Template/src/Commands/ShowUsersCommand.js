e76c397c7c734730b40b13cd6d97897452484c1d
Ilia Popov <ilia_popov02@abv.bg>
2023-06-06 13:37:54 +0300

    Add ShowUsersCommand class

--- Template/src/Commands/ShowUsersCommand.js
+++ Template/src/Commands/ShowUsersCommand.js
@@ -1,6 +1,20 @@
 ﻿import { BaseCommand } from './BaseCommand.js';
 
+export class ShowUsersCommand extends BaseCommand {
 
-export class ShowUsersCommand {
-	// ToDo
+  constructor(commandParameters, repository) {
+    super(commandParameters, repository);
+  }
+
+  executeCommand() {
+    if (this.repository.loggedUser.isAdmin()) {
+      return this.repository.users.reduce((userPrint, currUser) => {
+        userPrint += `${currUser.print()}\n`;
+
+        return userPrint;
+      }, '');
+    }
+
+    throw new Error('User must be admin to show all the users in the repository!');
+  }
 }
